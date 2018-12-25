b89dc8581b093287f30463b1b9cb49d4e5772db4
Sebastian Schneider <sebastian.schneider@maibornwolff.de>
2018-12-25 15:14:19 +0100

    Deleting old GUI elements

--- src/com/wachs/main/viewLayerJavaFX/MainWindow.java
+++ src/com/wachs/main/viewLayerJavaFX/MainWindow.java
@@ -23,16 +23,13 @@ public class MainWindow extends Application {
         //primaryStage.setMaximized(true);
 
 
-        Scene scene = new Scene(root, 1440, 900);
+        Scene scene = new Scene(root);
         scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
 
-
+        primaryStage.sizeToScene();
         primaryStage.show();
-        primaryStage.setMinWidth(1440);
-        primaryStage.setMinHeight(900);
-        primaryStage.setMaxWidth(1440);
-        primaryStage.setMaxHeight(900);
+        primaryStage.sizeToScene();
     }
 }

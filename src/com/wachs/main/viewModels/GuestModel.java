efc2d20659e1f14926a1ba7e26dcca63e4703454
Sebastian Schneider <sebastian.schneider@maibornwolff.de>
2019-02-25 17:29:39 +0100

    Refacotring model ... still to do --> DAO and QueryGenerator and GUI Adjusting

--- src/com/wachs/main/viewModels/GuestModel.java
+++ src/com/wachs/main/viewModels/GuestModel.java
@@ -2,22 +2,29 @@
 
 package com.wachs.main.viewModels;
 
+import com.wachs.main.businessObjects.Drinks;
 import com.wachs.main.businessObjects.Guest;
 import com.wachs.main.businessObjects.Project;
-import com.wachs.main.dataAccess.DAO.GuestDAO;
-import com.wachs.main.dataAccess.DAO.GuestDAOImpl;
-import com.wachs.main.dataAccess.DAO.ProjectDAO;
-import com.wachs.main.dataAccess.DAO.ProjectDAOImpl;
+import com.wachs.main.businessObjects.Stay;
+import com.wachs.main.dataAccess.DAO.*;
+
+import java.util.ArrayList;
 
 public class GuestModel {
 
     private GuestDAO guestDAO;
     private ProjectDAO projectDAO;
+    private StayDAO stayDAO;
+    private DrinksDAO drinksDAO;
+    private DrinksExpensesDAO drinksExpensesDAO;
+    private FoodExpensesDAO foodExpensesDAO;
     private Guest searchedGuest;
     private StayModel staySearchedGuest;
     private DrinksModel drinksSearchedGuest;
     private FoodModel foodSearchedGuest;
     private PrepaidModel prepaidSearchedGuest;
+    private FoodExpenseModel foodExpensesSearchedGuest;
+    private DrinksExpenseModel drinksExpensesSearchedGuest;
     private Project searchedProject;
     private int idProject;
     private int idGuest;
@@ -38,6 +45,11 @@ public class GuestModel {
         searchedGuest = guestDAO.findOneGuestByOneProject(guestName, idProject);
         idGuest = searchedGuest.getPK_id();
 
+        stayDAO = new StayDAOImpl();
+        drinksDAO = new DrinksDAOImpl();
+        drinksExpensesDAO = new DrinksExpensesDAOImpl();
+        foodExpensesDAO = new FoodExpensesDAOImpl();
+
         //get numbers of Guests
         countGuestSelectedProject = guestDAO.findAllGuestsByOneProject(idProject).size();
 
@@ -45,6 +57,8 @@ public class GuestModel {
         drinksSearchedGuest = new DrinksModel(idGuest, idProject);
         foodSearchedGuest = new FoodModel(idGuest, idProject);
         prepaidSearchedGuest = new PrepaidModel(idGuest, idProject);
+        foodExpensesSearchedGuest = new FoodExpenseModel(idGuest, idProject);
+        drinksExpensesSearchedGuest = new DrinksExpenseModel(idGuest, idProject);
 
 
     }
@@ -56,7 +70,7 @@ public class GuestModel {
 
     public double getSleepOverEUR() {
 
-        return 0;
+        return (this.getRent() * (this.getSleeopover() / (this.getAllSleepOverOneProject())));
     }
 
     public int getDrinksCount() {
@@ -66,7 +80,7 @@ public class GuestModel {
 
     public double getDrinksEUR() {
 
-        return 0;
+        return this.getAllDrinksExpensesOneProject() * (this.getDrinksCount() / this.getAllDrinksOneProject());
     }
 
     public int getFoodCount() {
@@ -98,5 +112,40 @@ public class GuestModel {
 
         return (searchedProject.getProjectPrice() / countGuestSelectedProject);
 
+    }
+
+    private int getAllSleepOverOneProject() {
+
+        int allNightsCountSelectedProject = 0;
+        ArrayList<Stay> staySelectedProject = stayDAO.findAllStayByOneProject(idProject);
+
+        for (Stay stay : staySelectedProject) {
+
+            allNightsCountSelectedProject = allNightsCountSelectedProject + stay.getNights();
+        }
+
+        return allNightsCountSelectedProject;
+
+    }
+
+    private int getAllDrinksOneProject() {
+
+        int allDrinksCountSelectedProject = 0;
+        ArrayList<Drinks> drinksSelectedProject = drinksDAO.findAllDrinksByOneProject(idProject);
+
+        for (Drinks drinks : drinksSelectedProject) {
+
+            allDrinksCountSelectedProject = allDrinksCountSelectedProject + drinks.getNights();
+        }
+
+        return allDrinksCountSelectedProject;
+    }
+
+    private double getAllDrinksExpensesOneProject() {
+
+        double allDrinksExpenses = 0;
+
+
+        return allDrinksExpenses;
     }
 }
